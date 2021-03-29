b4e7d81a7162d2dfbb9669b648a24106c9838e9f
SARAGOCA <glaydson.saragoca@gmail.com>
2021-03-28 22:55:04 -0300

    initial commit

---
+++ dist/inject.js
@@ -0,0 +1,9 @@
+// import .js scripts into DOM
+
+// EXAMPLE 
+// const scriptToInject = document.createElement('script');
+// scriptToInject.src = chrome.extension.getURL('xhr.js');
+// scriptToInject.onload = function() {
+//     this.remove();
+// };
+// (document.head || document.documentElement).appendChild(scriptToInject);
